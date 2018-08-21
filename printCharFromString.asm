2b9bb91b7e3c7cbbceda431ea3743dc694435332
bdhagger <38987202+bdhagger@users.noreply.github.com>
2018-08-20 19:28:41 -0700

    Update printCharFromString.asm

--- printCharFromString.asm
+++ printCharFromString.asm
@@ -4,15 +4,15 @@
 
 .text
  
-la       $t1 array   # puts first address of array into $t0
-move     $a0 $t1     # intitialize address of current array location
-li       $v0 4       # initialize array offset
+la       $t1 array   # puts first address of array into $t1
+move     $a0 $t1     
+li       $v0 4       # prints "David"
 syscall
 
-la       $a0 nl
+la       $a0 nl      # prints a new line
 syscall
 
-la      $t2 array
-lb      $a0 3($t2)
+la      $t2 array    
+lb      $a0 3($t2)   # prints array[3] which is "i"
 li      $v0 11
 syscall
