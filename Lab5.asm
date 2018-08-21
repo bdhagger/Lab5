002a2827168530531182f1a7ab83f4ae88c74e86
bdhagger <38987202+bdhagger@users.noreply.github.com>
2018-08-20 17:30:33 -0700

    compares first character to 1

--- Lab5.asm
+++ Lab5.asm
@@ -16,7 +16,6 @@
 #
 #########################################################################################
 
-#########################################################################################
 # PSEUDO CODE
 
 # read and store the 8-bit number string
@@ -44,19 +43,26 @@
 #########################################################################################
 
 # REGISTER USAGE
+
 # a0 stores the ascii strings to be printed
 # a1 is the first program argument
+
 # t0 holds the first program argument
+# t1 holds the first character of the program argument
+
 # s0 stores the 32-bit sign extended value entered by the user
 # v0 sets the syscalls to print strings and characters only
 
 #########################################################################################
+
+# PROGRAM
 
 .data 
        bn: .asciiz "You entered the binary number:\n"
        hx: .asciiz "The hex representation of the sign-extended number is:\n"
        dc: .asciiz "The number in decimal is:\n"
        nl: .asciiz "\n\n"
+       one: .asciiz "1"
        
 
 
@@ -70,16 +76,47 @@
        li      $v0 4
        syscall
 
-       la      $a0 nl        # print a new line
+       la      $a0 nl       # print a new line
        li      $v0 4
        syscall
        
-       lb      $t1 ($t0)     # look at first character of arg string
+       lb      $t1 ($t0)    # put first character of arg string into t1
        move    $a0 $t1
        li      $v0 11
        syscall
        
-       li      $s0 0         # initialize s0 to zero
+       la      $a0 nl       # print a new line
+       li      $v0 4
+       syscall
+       
+       
+       lb      $t2 one      # load string "1" into t2
+       move    $a0 $t2
+       li      $v0 11
+       syscall
+       
+       li      $s0 0        # initialize s0 to zero
+       
+       beq     $t1 $t2 rEq
+       
+back1: 
+       la      $a0 nl        # print a new line
+       li      $v0 4
+       syscall
+       
+       move $a0 $s0
+       li $v0 1
+       syscall
+       
+       li      $v0 10
+       syscall
+     
+rEq:
+       add $s0 $s0 -256      
+       j back1
+       
+       
+
   
  # move    $a0 $s0     # print hex message
  # li      $v0 11
