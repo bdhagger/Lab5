7a4788b434667248089f3c2ab4b71900ac55dcc3
bdhagger <38987202+bdhagger@users.noreply.github.com>
2018-08-20 19:06:04 -0700

    Add files via upload

--- arrayPrinted.asm
+++ arrayPrinted.asm
@@ -1,42 +1,24 @@
 .data 
-  
-       sl: .asciiz "Hello world.\n"
-
+ array: .word 8      # allocates 8 bytes of memory in data
+ 
 .text
-       la     $t2 sl
-       move   $a0 $t2
-       li     $v0 4
-       syscall
-       
-       la     $t2 sl
-       lb     $a0 6($t2)
-       li     $v0 11
-       syscall
-  
+ 
+la       $t0 array   # puts first address of array into $t0
+li       $t1 0       # initialize array offset
+li       $t3 0       # initialize value to store
+move     $t2 $t0     # intitialize address of current array location
 
+loop:
 
-  
+sb      $t3 ($t2)    # put lsb of $t3 into memory address $t2
+addi    $t3 $t3 1    # increment value
+addi    $t1 $t1 4    # increment array offset
+add     $t2 $t0 $t1  # update address
 
+move    $a0 $t3      # print values in array
+li      $v0 1
+syscall
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+blt     $t1 32 loop  # conditional statement
+li      $v0 10
+syscall
